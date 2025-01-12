771508b234d2a63e23daca9b32cfc27f61d413f3
Megan Bougle <mmbougle@uamv.edu.ni>
2025-01-12 12:36:41 -0600

    Added Diagonal Difference problem

---
+++ Problem Solving/diagonalDifference.cpp
@@ -0,0 +1,109 @@
+//Solved using HackerRank, code may not run on your machine beacuse test cases are missing
+//https://www.hackerrank.com/challenges/diagonal-difference/problem
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'diagonalDifference' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts 2D_INTEGER_ARRAY arr as parameter.
+ */
+
+int diagonalDifference(vector<vector<int>> arr) {
+    int n= arr.size();
+
+     int primary = 0;
+     int secondary = 0;
+    
+    // Sum the diagonal elements
+    for (int i = 0; i < n; i++) {
+       
+        primary+= arr[i][i]; //The primary diagonal is always eQual rows and columns, example for a 3x3 matrix [1][1],[2][2],[3][3]
+        secondary += arr[i][n-i-1]; //The secondary diagonal is the row index and the column index n-i-1
+        // Meaning I am practically iterating backwards, example for a 3x3 matrix the first element of the secondary
+        //diagonal would be the following: i=0,n=3 [0][3-0-1]= [0][2] gives the first row, last column
+    }
+    
+    return abs(primary-secondary);
+    
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string n_temp;
+    getline(cin, n_temp);
+
+    int n = stoi(ltrim(rtrim(n_temp)));
+
+    vector<vector<int>> arr(n);
+
+    for (int i = 0; i < n; i++) {
+        arr[i].resize(n);
+
+        string arr_row_temp_temp;
+        getline(cin, arr_row_temp_temp);
+
+        vector<string> arr_row_temp = split(rtrim(arr_row_temp_temp));
+
+        for (int j = 0; j < n; j++) {
+            int arr_row_item = stoi(arr_row_temp[j]);
+
+            arr[i][j] = arr_row_item;
+        }
+    }
+
+    int result = diagonalDifference(arr);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
